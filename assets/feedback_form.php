dee947572afd1a19e557da827d99d89005799c5b
zhannakitbalyan <78276765+zhannakitbalyan@users.noreply.github.com>
2021-01-30 14:57:16 -0500

    Add files via upload

---
+++ assets/feedback_form.php
@@ -0,0 +1,21 @@
+
+
+
+<?php
+if(isset($_POST["submit"])) {
+$recipient = "eve.kazarian@gmail.com"; //my email
+echo $subject = 'Email message from Point Plumbing';
+echo $name = $_POST ["yourName"];
+echo $email = $_POST["yourEmail"];
+echo $phone = $_POST["yourPhone"];
+echo $location = $_POST["yourLocate"];
+echo  $message = $_POST["yourMessage"];
+
+ $mailBody="Name: $name\nEmail: $email\n\n$message"; 
+
+ mail($recipient, $subject, $mailBody, "From: $name <$email>");
+
+echo $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
+
+}
+?>
